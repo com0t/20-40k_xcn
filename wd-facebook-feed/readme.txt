0f2989e9bc512e7a9f2bcbe8c7c32eeb6bdbe4a0
com0tf3 <com0tf3@gmail.com>
2021-12-02 08:43:42 +0000

    first commit

---
+++ wd-facebook-feed/readme.txt
@@ -0,0 +1,9 @@
+=== 10Web Social Post Feed===
+Contributors: webdorado,10web,wdsupport
+Tags: customizable facebook feed, facebook, facebook feed, facebook group, facebook like box, facebook likes, facebook page, facebook photos, facebook plugin, facebook posts, facebook likebox
+Requires at least: 3.4
+Requires PHP: 5.2
+Tested up to: 5.8
+Stable tag: 1.2.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
---
+++ webappick-product-feed-for-woocommerce/admin/js/woo-feed-facebook-pixel.js
@@ -0,0 +1,19 @@
+(function($, wp) {
+
+    "use strict";
+    $(document).ready(function(){
+        if( typeof fbq === "function" ){
+            $(this.body).on( 'added_to_cart', function( event, fragments, cart_hash, button ){
+                var product_id = button.data('product_id');
+                wp.ajax.post('add_to_cart_facebook_pixel', {product_id: product_id})
+                    .done(function(response){
+                        fbq( 'track', 'AddToCart', response );
+                    })
+                    .fail(function (){
+                        console.log( "Request Failed!" );
+                    });
+            } );
+        }
+    });
+
+})(jQuery, wp);
